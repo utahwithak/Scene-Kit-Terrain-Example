aa9455c450e5622c975c67f6ee1d2b2cf5c0dd1d
Carl Wieland <carlwieland@me.com>
2015-01-21 19:52:44 -0800

    Initial Commit

---
+++ SceneExample/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  SceneExample
+//
+//  Created by Carl Wieland on 1/21/15.
+//  Copyright (c) 2015 Carl Wieland. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+    
+}
